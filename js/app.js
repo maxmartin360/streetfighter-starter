8a53548aa4fa49e427b5f474e8fd974f67e8909e
Max Martin <maxmartin360@gmail.com>
2015-04-26 20:52:21 -0400

    final commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+	$('.ryu-still').show();
+  })
+  .mousedown(function() {
+  	playHadouken();
+  	$('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+  	$('.hadouken').finish().show().animate(
+  {'left': '1020px'},
+  500,
+  function() {
+    $(this).hide();
+    $(this).css('left', '624px');
+  }
+);
+  })
+  .mouseup(function() {
+  	$('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+  	// ryu goes back to his ready position
+  });
+});
+ $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-ready').hide();
+      $('.ryu-still').hide();
+      $('.ryu-action').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
